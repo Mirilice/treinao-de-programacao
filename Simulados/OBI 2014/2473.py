d71bc4ca06cc0c3b4892449ce5f1b54a07561abc
Mirilice <mariaalicepco10@gmail.com>
2025-04-12 01:51:14 -0300

    new exercise

---
+++ Simulados/OBI 2014/2473.py
@@ -0,0 +1,24 @@
+# Flavinho sabe que a chance de ganhar na loteria é bem pequena. Ele gosta muito de estudar probabilidade! Mas, justamente por entender de probabilidades, Flavinho segue o ditado, “quem não arrisca, não petisca!”, e faz um jogo toda semana.
+
+# Na loteria preferida dele, o jogador aposta seis números entre 1 e 99. No sorteio, também são escolhidos seis números ganhadores entre 1 e 99. Quem acerta 3, 4, 5 ou 6 números ganha como prêmio, respectivamente, um “terno”, uma “quadra”, uma “quina” ou uma “sena”.
+
+# Nesta tarefa, você deve escrever um programa que diga qual foi o prêmio que Flavinho ganhou, dados os seis números que ele apostou e os seis números que foram sorteados.
+
+# Entrada
+# A entrada consiste de duas linhas apenas. Na primeira linha são dados seis números inteiros distintos entre 1 e 99, representando a aposta do Flavinho. A segunda linha contém os seis números inteiros distintos sorteados.
+
+# Saída
+# Seu programa deve imprimir uma linha contendo uma palavra: “terno”, “quadra”, “quina” ou “sena”; caso Flavinho tenha acertado, respectivamente, 3, 4, 5, ou 6 números. Caso ele tenha acertado menos do que 3 números, imprima a palavra “azar”.
+
+jogo = set(map(int, input().split()))
+sorteio = set(map(int, input().split()))
+acertos = jogo & sorteio #interseção
+if len(acertos) == 3:
+    print("terno")
+elif len(acertos) == 4:
+    print("quadra")
+elif len(acertos) == 5:
+    print("quina")
+elif len(acertos) == 6:
+    print("sena")
+else: print("azar")
