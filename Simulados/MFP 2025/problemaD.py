7aaf4122075e66ecf1bc7f002e1a815e37d0f3b1
Mirilice <mariaalicepco10@gmail.com>
2025-05-12 22:22:58 -0300

    ajuda na primeira fase

--- Simulados/MFP 2025/problemaD.py
+++ Simulados/MFP 2025/problemaD.py
@@ -1,8 +1,8 @@
-# import math
-
+#com ajuda do monitor:
+# pi = 3.14159265359
 # R = list(map(int, input().split()))
 # N, Q = map(int, input().split())
-
+ 
 # for _ in range(Q):
 #     C1, L1, C2, L2 = map(int, input().split())
 #     C1 -= 1
@@ -14,9 +14,8 @@
 #     dist_radial = abs(r1 - r2)
 #     dlinha = abs(L1 - L2)
 #     dlinha = min(dlinha, N - dlinha)
-#     circ1 = 2 * math.pi * r1
-#     circ2 = 2 * math.pi * r2
-#     caminho1 = (circ1 * dlinha / N) + dist_radial
-#     caminho2 = (circ2 * dlinha / N) + dist_radial
-#     resultado = min(caminho1, caminho2)
-#     print(f"{resultado:.9f}")
+#     resultado = float('inf')
+#     for r in R:
+#         caminho = abs(r1 - r) + abs(r2 - r) + (2 * pi * r * dlinha / N)
+#         resultado = min(resultado, caminho)
+#     print(f"{resultado:.10f}")
--- Simulados/MFP 2025/problemaE.py
+++ Simulados/MFP 2025/problemaE.py
@@ -1,1 +1,22 @@
-#não consegui entender
+#depois da explicação do monitor
+N, M = map(int, input().split())
+matriz = []
+for i in range(N):
+    linha = list(map(int, input().split()))
+    matriz.append(linha)
+
+for i in range(N):
+    for j in range(M):
+        valor = i + j 
+        if valor % 2 == 0:
+            if matriz[i][j]  % 2 != 0:
+                matriz[i][j] +=1 
+        else:
+            if matriz[i][j] % 2== 0:
+                matriz[i][j] += 1
+                
+for i in range(N):
+    for j in range(M):
+        print(matriz[i][j], end=" ")
+    print()
+    
